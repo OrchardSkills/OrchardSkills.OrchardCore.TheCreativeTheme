ca09c70f208817fb86328597fe62f6d1b288a1e6
sales <sales@orchardskills.com>
2020-08-22 12:50:00 -0600

    Added updated code

---
+++ OrchardSkills.OrchardCore.TheCreativeTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The OrchardSkills Creative Theme",
+    Author = "Orchard Skills",
+    Website = "https://OrchardSkills.com",
+    Version = "1.0.0-rc2",
+    Description = "An Orchard Skills theme adapted for Creative websites."
+)]
